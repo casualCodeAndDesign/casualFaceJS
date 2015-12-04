c479a3d3ee98619121088226161eb875af608d5e
Kämppä,Joni J.M <j.kamppa@student.fontys.nl>
2015-12-04 11:24:36 +0100

    Removed crossdomain, changed JS code

--- js/face.js
+++ js/face.js
@@ -1,10 +1,18 @@
-FB.api(
-    "/BrewDogHel",
-    'GET',
-    {"fields":"posts.limit(10)"},
-    function (response) {
-        if (response && !response.error) {
-            console.log("Success....")
+function facebookInit() {
+    // Initialize Facebook Connection
+    FB.api(
+        "/BrewDogHel",
+        'GET',
+        {"fields":"posts.limit(10)"},
+        function (response) {
+            if (response && !response.error) {
+                console.log("Success....")
+                var faceSection = document.getElementById("facebook");
+                var node = document.createElement("P");
+                var textNode = document.createTextNode("Success...");
+                node.appendChild(textNode);
+                faceSection.appendChild(node);
+            }
         }
-    }
-);
+    );
+};
